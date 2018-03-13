54dd112fc822afcd4f3fdd5c60c0ce6f479de865
arman.sharvel <arman@v3.agency>
2018-03-13 15:34:53 +0300

    Initial Commit

---
+++ FallingWords/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FallingWords
+//
+//  Created by Arman Arutyunov on 13/03/2018.
+//  Copyright © 2018 Arman Arutyunov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
